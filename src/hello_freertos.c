157966a15f41c1d1a88fb6e2a3f7b00a025c3cf6
Tyler <gourley18@gmail.com>
2025-09-10 13:10:46 -0600

    added comments to document the funtionality of hello_freertos.c

--- src/hello_freertos.c
+++ src/hello_freertos.c
@@ -2,6 +2,8 @@
  * Copyright (c) 2022 Raspberry Pi (Trading) Ltd.
  *
  * SPDX-License-Identifier: BSD-3-Clause
+ * 
+ * This file helps blinks an LED using FreeRTOS OS scheduler
  */
 
 #include <stdio.h>
@@ -21,19 +23,24 @@ bool on = false;
 #define MAIN_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 #define BLINK_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 
+// Blink thread blinks with delay of 500ms
 void blink_task(__unused void *params) {
     hard_assert(cyw43_arch_init() == PICO_OK);
     while (true) {
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on);
-        if (count++ % 11) on = !on;
-        vTaskDelay(500);
+        if (count++ % 11) on = !on; // Possible undefined behavior with int overflow with count
+        vTaskDelay(500); // waits 500ms for scheduler touches it
     }
 }
 
+// Spawn Blink thread and run task
 void main_task(__unused void *params) {
+    // Create a blink thread
     xTaskCreate(blink_task, "BlinkThread",
                 BLINK_TASK_STACK_SIZE, NULL, BLINK_TASK_PRIORITY, NULL);
     char c;
+    // Lowercase letter input outputs Uppercase letter and vice versa
+    // Non-alphabet input chars are just output as the character
     while(c = getchar()) {
         if (c <= 'z' && c >= 'a') putchar(c - 32);
         else if (c >= 'A' && c <= 'Z') putchar(c + 32);
@@ -47,8 +54,9 @@ int main( void )
     const char *rtos_name;
     rtos_name = "FreeRTOS";
     TaskHandle_t task;
+    // Create Main thead which spawns the blink thread
     xTaskCreate(main_task, "MainThread",
                 MAIN_TASK_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, &task);
     vTaskStartScheduler();
-    return 0;
+    return 0; // kills the initial main thread that was spawned at int main()
 }
